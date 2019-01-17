3a5d308b2d5031e36a9d94e794ad552329805db2
xakakeshita <k.kakeshita@xearts.jp>
2019-01-17 18:02:17 +0900

    スタイルを適用させる

--- public_html/wp-can-b/wp-content/themes/can-b/archive.php
+++ public_html/wp-can-b/wp-content/themes/can-b/archive.php
@@ -13,7 +13,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-      <div class="st-Content">
+      <div class="st-Content_Inner">
 
         <?php if ( have_posts() ) : ?>
 
--- public_html/wp-can-b/wp-content/themes/can-b/template-parts/content.php
+++ public_html/wp-can-b/wp-content/themes/can-b/template-parts/content.php
@@ -9,51 +9,32 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('archive-Example_Unit'); ?>>
+
 	<header class="entry-header">
-<!--		--><?php
-//		if ( is_singular() ) :
-//			the_title( '<h1 class="entry-title">', '</h1>' );
-//		else :
-//			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-//		endif;
-//
-//		if ( 'post' === get_post_type() ) :
-//			?>
-<!--			<div class="entry-meta">-->
-<!--				--><?php
-//				can_b_posted_on();
-//				can_b_posted_by();
-//				?>
-<!--			</div><!-- .entry-meta -->
-<!--		--><?php //endif; ?>
+
 	</header><!-- .entry-header -->
 
-	<?php can_b_post_thumbnail(); ?>
-
 	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'can-b' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'can-b' ),
-			'after'  => '</div>',
-		) );
-		?>
+    <a href="<?php echo get_permalink( ); ?>">
+
+      <div class="archive-Example_Item">
+
+        <figure class="archive-Example_ImgUnit">
+          <?php the_post_thumbnail(); ?>
+        </figure>
+        <h3 class="archive-Example_Title">
+          <?php echo get_the_title(); ?>
+        </h3>
+
+      </div>
+
+    </a>
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php can_b_entry_footer(); ?>
+    <!--		--><?php //can_b_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
