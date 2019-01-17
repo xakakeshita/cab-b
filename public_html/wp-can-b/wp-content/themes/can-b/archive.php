7062bda9ae669eb064e91977883de1be8480c1db
xakakeshita <k.kakeshita@xearts.jp>
2019-01-17 14:24:18 +0900

    archiveページ｜編集

--- public_html/wp-can-b/wp-content/themes/can-b/archive.php
+++ public_html/wp-can-b/wp-content/themes/can-b/archive.php
@@ -13,37 +13,41 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+      <div class="st-Content">
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+        <?php if ( have_posts() ) : ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+          <header class="page-header">
+            <?php
+            the_archive_title( '<h1 class="page-title">', '</h1>' );
+            the_archive_description( '<div class="archive-description">', '</div>' );
+            ?>
+          </header><!-- .page-header -->
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+          <?php
+          /* Start the Loop */
+          while ( have_posts() ) :
+            the_post();
 
-			endwhile;
+            /*
+             * Include the Post-Type-specific template for the content.
+             * If you want to override this in a child theme, then include a file
+             * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+             */
+            get_template_part( 'template-parts/content', get_post_type() );
 
-			the_posts_navigation();
+          endwhile;
 
-		else :
+          the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+        else :
 
-		endif;
-		?>
+          get_template_part( 'template-parts/content', 'none' );
+
+        endif;
+        ?>
+
+      </div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
